784b4d4f4e7898e9fd19da4d3f75c5bee03de3db
Brutalbeard <jceloria@gmail.com>
2016-04-02 22:01:01 -0600

    Just trying things

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 uri = URI.parse(ENV["redis://h:p7jajl987e24uc5g5t45hu21ad@ec2-54-235-164-4.compute-1.amazonaws.com:20849"])
-$redis = Redis.new(:host => ec2-54-235-164-4.compute-1.amazonaws.com, :port => 20849, :password => "p7jajl987e24uc5g5t45hu21ad")
+$redis = Redis.new(:host => ec2-54-235-164-4.compute-1.amazonaws.com, :port => 20849, :password => p7jajl987e24uc5g5t45hu21ad)
 
 
 #$redis = Redis.new(url: ENV["redis://h:p7jajl987e24uc5g5t45hu21ad@ec2-54-235-164-4.compute-1.amazonaws.com:2084"])
