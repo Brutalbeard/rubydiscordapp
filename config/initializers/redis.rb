a5d52bc01dc542cdaa7f56d1a3e5aa62e1e81fb9
Brutalbeard <jceloria@gmail.com>
2016-04-02 21:21:45 -0600

    Stuff

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,1 @@
-$redis = Redis.new(url: ENV["rredis://h:p7jajl987e24uc5g5t45hu21ad@ec2-54-235-164-4.compute-1.amazonaws.com:2084"])
+$redis = Redis.new(url: ENV["redis://h:p7jajl987e24uc5g5t45hu21ad@ec2-54-235-164-4.compute-1.amazonaws.com:2084"])
