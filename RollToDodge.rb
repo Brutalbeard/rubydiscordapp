1c1d18f6ae1100aad4bae610d82beffb02757c46
Brutalbeard <jceloria@gmail.com>
2016-04-06 15:29:22 -0400

    This is the version that works in the CLI

--- RollToDodge.rb
+++ RollToDodge.rb
@@ -187,8 +187,8 @@ end
 bot.command(:makeMe, description: "Initializes your character sheet", usage: "/makeMe Connor") do |event, *args|
   player = event.user.id
   givenName = args.join(' ')
-  $redis.set player (name => givenName)
-  $redis.get player name
+  $redis.set player:name givenName
+  $redis.get player:name
 end
 
 bot.command(:makeStat, description: "Generates a stat, checks for preexisting.", usage: "/makeStat con 10") do |event, *args|
