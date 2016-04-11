397cf170a50556c7eb25f3ea616c7ec27dd90992
Brutalbeard <jceloria@gmail.com>
2016-04-10 23:45:53 -0400

    Still prettying this up, and making a function for returning rolls...

--- FunctionsAndMethods.rb
+++ FunctionsAndMethods.rb
@@ -1,19 +1,16 @@
-def showAll(player) #checks for a valid attribute, and returns that as lower case
-  dex = $redis.get "#{player}:dex"
-  name = $redis.get "#{player}:name"
-  con = $redis.get "#{player}:con"
-  int = $redis.get "#{player}:int"
-  wis = $redis.get "#{player}:wis"
-  str = $redis.get "#{player}:str"
-  cha = $redis.get "#{player}:cha"
-  """Name: #{name}
-  Dexterity: #{dex}
-  Constitution: #{con}
-  Intelligence: #{int}
-  Wisdom: #{wis}
-  Strength: #{str}
-  Charisma: #{cha}"""
+def rollNoBonus(player, diceAmount, diceType) #checks for a valid attribute, and returns that as lower case
+  rolls = Array.new()
+  text = String.new()
+  text << "#{player} rolled #{diceAmount}, #{diceType} sided die...\n"
+  totRoll = 0
+ for i in 1..(diceAmount)
+  rolls[i] = (rand(1..diceType))
+  totRoll += rolls[i]
+  text << "Roll #{i}: #{rolls[i]} \n"
+ end
+text << "\nTotal: #{totRoll}"
 end
 
-input = gets.chomp
-fixed = statCheck(input)
+input1 = gets.chomp.to_i
+input2 = gets.chomp.to_i
+puts rollOff(input1, input2)
--- RollToDodge.rb
+++ RollToDodge.rb
@@ -44,7 +44,7 @@ def bonuses(player, stat) #reusable stat bonus
     return (number.to_i-10)/2
 end
 
-def showAll(player) #takes the player ID, and give back all their stats. Had this elswhere, looks prettier here. 
+def showAll(player) #takes the player ID, and give back all their stats. Had this elswhere, looks prettier here.
   dex = $redis.get "#{player}:dex"
   name = $redis.get "#{player}:name"
   con = $redis.get "#{player}:con"
@@ -52,13 +52,26 @@ def showAll(player) #takes the player ID, and give back all their stats. Had thi
   wis = $redis.get "#{player}:wis"
   str = $redis.get "#{player}:str"
   cha = $redis.get "#{player}:cha"
-  """Name: #{name}
-  Dexterity: #{dex}
-  Constitution: #{con}
-  Intelligence: #{int}
-  Wisdom: #{wis}
-  Strength: #{str}
-  Charisma: #{cha}"""
+  "Name: #{name}\n
+  Dexterity: #{dex}\n
+  Constitution: #{con}\n
+  Intelligence: #{int}\n
+  Wisdom: #{wis}\n
+  Strength: #{str}\n
+  Charisma: #{cha}"
+end
+
+def rollNoBonus(player, diceAmount, diceType) #Does the work to do a roll that doesn't include a bonus
+  rolls = Array.new()
+  text = String.new()
+  text << "#{player} rolled #{diceAmount}, #{diceType} sided die...\n"
+  totRoll = 0
+ for i in 1..(diceAmount)
+  rolls[i] = (rand(1..diceType))
+  totRoll += rolls[i]
+  text << "Roll #{i}: #{rolls[i]} \n"
+ end
+ text
 end
 
 bot.message(from: not!("Iblan"), containing: "Suck it Ian!") do |event| #Will probably make this cooler. You'll see.
@@ -74,21 +87,14 @@ bot.command(:shoot, description: "Shoots and arrow at whoever, or whatever you w
 end
 
 bot.command(:roll, description: "Returns a roll.", usage: "Type /roll 1d20 as an example") do |event, arg| # so the description and the usage are both for help. That's something the message above doesn't have. Event means, it happened I guess? Little fuzzy there. Then the 'arg' is whatever they type in after calling the command. Which runs through old faithful down below.
+  player = event.user.id
+  name = $redis.get "#{player}:name"
   if arg.match(/\d{1,}[d]\d{1,2}/) == nil
     text = "Wrong syntax. Try /help roll"
   else
     diceAmount = arg.split("d")[0].to_i
     diceType = arg.split("d")[1].to_i
-    rolls = Array.new()
-    text = String.new()
-    text << "#{event.user.name} rolled #{diceAmount}, #{diceType} sided die...\n"
-    totRoll = 0
-   for i in 1..(diceAmount)
-    rolls[i] = (rand(1..diceType))
-    totRoll += rolls[i]
-    text << "Roll #{i}: #{rolls[i]} \n"
-   end
-  text << "\nTotal: #{totRoll}"
+    rollNoBonus(name, diceAmount, diceType)
   end
   text #so this also differs from the messages above. Don't have to put event.respond. That's what was causing those double responses earlier. Just put the variable adter the last 'end' which closes out the 'do' at the top. Then it sends back that variable. Boom.
 end
